96488211b2b1f82cacf96a632e2ebaaea5350f81
Vladimir <eftwenty@gmail.com>
2020-05-03 02:44:52 +0300

    Added synchronous file logger

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'slim'
 
 class App < Sinatra::Base
+  configure do
+    FileUtils.mkdir_p('log') unless File.directory? 'log'
+    file = File.new("log/app.log","a+")
+    file.sync = true
+
+    use Rack::CommonLogger, file
+  end
+
   get '/' do
     response = octokit_client.search_repositories 'spr_t'
     @items = response.items
